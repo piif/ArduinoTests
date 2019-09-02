1ca6a9f1ad7659832cd580b5ca4b254fbab723cb
piif <christian_lefebvre@laposte.net>
2019-09-02 23:08:42 +0200

    use port registers instead of lib functions

--- leds/testLeds.ino
+++ leds/testLeds.ino
@@ -22,6 +22,17 @@ setIntervalTimer animationTimer, displayTimer;
 // mask 0x20 0x10 0x08 0x04 0x02 0x01
 byte leds = 0;
 
+#define LED_1  0x20
+#define LED_2  0x10
+#define LED_3  0x08
+#define LED_1b 0x04
+#define LED_2b 0x02
+#define LED_3b 0x01
+
+#define OUT_A 0x08
+#define OUT_B 0x10
+#define OUT_C 0x20
+
 void animationStep(void *userData, long late, int missed) {
 	static byte bit = 1;
 	static bool on = 1;
@@ -48,28 +59,52 @@ void animationStep(void *userData, long late, int missed) {
 
 void displayStep(void *userData, long late, int missed) {
 	static byte step  = 0;
+	byte portd = PORTD & 0xC7, ddrd = DDRD  & 0xC7; // clear bits 3, 4 & 5 to copy to real PORTD and DDRD
 	char A,B,C;
 
 	switch(step) {
 	case 0:
-		A = '1';
-		B = (leds & 0x20) ? '0' : 'z';
-		C = (leds & 0x01) ? '0' : 'z';
+		// A = '1';
+		portd |= OUT_A; ddrd |= OUT_A;
+		// B = (leds & 0x20) ? '0' : 'z';
+		if (leds & LED_1) {
+			ddrd |= OUT_B;
+		} // else ddrd already cleared
+		// C = (leds & 0x01) ? '0' : 'z';
+		if (leds & LED_3b) {
+			ddrd |= OUT_C;
+		}
 	break;
 	case 1:
-		A = (leds & 0x04) ? '0' : 'z';
-		B = '1';
-		C = (leds & 0x10) ? '0' : 'z';
+		// A = (leds & 0x04) ? '0' : 'z';
+		if (leds & LED_1b) {
+			ddrd |= OUT_A;
+		}
+		// B = '1';
+		portd |= OUT_B; ddrd |= OUT_B;
+		// C = (leds & 0x10) ? '0' : 'z';
+		if (leds & LED_2) {
+			ddrd |= OUT_C;
+		}
 	break;
 	case 2:
-		A = (leds & 0x08) ? '0' : 'z';
-		B = (leds & 0x02) ? '0' : 'z';
-		C = '1';
+		// A = (leds & 0x08) ? '0' : 'z';
+		if (leds & LED_3) {
+			ddrd |= OUT_A;
+		}
+		// B = (leds & 0x02) ? '0' : 'z';
+		if (leds & LED_2b) {
+			ddrd |= OUT_B;
+		}
+		// C = '1';
+		portd |= OUT_C; ddrd |= OUT_C;
 	break;
 	}
-	parseBit(A,3);
-	parseBit(B,4);
-	parseBit(C,5);
+	PORTD = portd;
+	DDRD  = ddrd;
+//	parseBit(A,3);
+//	parseBit(B,4);
+//	parseBit(C,5);
 
 	step = (step + 1) % 3;
 }
@@ -81,7 +116,7 @@ void setup() {
 	pinMode(5, OUTPUT);
 
 	displayTimer = setInterval(7, displayStep, NULL);
-	animationTimer = setInterval(50, animationStep, NULL);
+	animationTimer = setInterval(250, animationStep, NULL);
 
 	Serial.println("ready");
 }
