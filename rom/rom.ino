fe81766528272eb9935b3e8a52dcd2a44cb5f528
piif <christian_lefebvre@laposte.net>
2022-01-02 22:52:04 +0100

    dump ok thru 2x74164 to handle address

--- rom/rom.ino
+++ rom/rom.ino
@@ -2,46 +2,53 @@
 // from datasheet https://pdf1.alldatasheet.com/datasheet-pdf/view/74503/MCNIX/27C256-10.html
 
 #include <Arduino.h>
+#include "serialInput.h"
 
 /**
  * wiring :
- *  A0-A7 = Q0-Q7
- *  A0 -  A5 -> D14 - D19 = PC0-5
- *  A5 - A11 -> D2  - D7  = PD2-7
- * A12 - A14 -> D8  - D10 = PB0-2
- * OE = D11 = PB3
- * CE = D12 = PB4
+ *  Output Enable (G) = A0 = PC0
+ *  Chip Enable (E)   = A1 = PC1
+ *  Q0-Q3 = D4-7 = PD4-7
+ *  Q4-7  = D8-11 = PB0-3
+ *  Adr0 -  Adr7 -> first 74164 outputs A-H
+ *  Adr8 - Adr14 -> second 74164 outputs A-G
+ *  first 74164 outputs H link to a+B inputs of second one
+ *  74164 Clocks = A2 = PC2
+ *  1st 74164 A+B inputs = A3 = PC3
  */
 
-byte readData(word address) {
-	// switch A0-A7 to output
-	DDRC |= 0x3F; // bits 0-5 output
-	DDRD |= 0x03; // bits 2-7 output
-	PORTB = PORTB | 0x18; // set CE and OE
+#define ADDR_CLOCK    A2
+#define ADDR_DATA     A3
+#define CHIP_ENABLE   A1
+#define OUTPUT_ENABLE A0
 
-	// fill addresss
-	PORTC = (PORTC & ~0x3F) | ( address        & 0x3F);
-	PORTD = (PORTD & ~0xFC) | ((address >>  4) & 0xFC); // >> 6 and << 2
-	PORTB = (PORTB & ~0x07) | ((address >> 12) & 0x07);
-
-	PORTB = PORTB & ~0x18; // clear CE and OE
-
-	// switch A0-A7 to input
-	PORTC &= ~0x3F;
-	DDRC &= ~0x3F; // bits 0-5 input without pull-up
-	PORTD &= ~0x03;
-	DDRD &= ~0x03; // bits 2-7 input without pull-up
-
-	return (PORTC & 0x3F) | ((PORTD & 0x03) << 6);
+void writeAddress(word addr) {
+	PORTC |= 0x02;
+	for(word mask = 0x8000; mask; mask >>= 1) {
+		if (mask & addr) {
+			PORTC |= 0x08;
+		} else {
+			PORTC &= ~0x08;
+		}
+		PORTC |= 0x04;
+		_NOP();
+		PORTC &= ~0x04;
+	}
+	PORTC &= ~0x02;
 }
 
-void setup() {
-	DDRC |= 0x3F; // bits 0-5 output
-	DDRD |= 0xFC; // bits 2-7 output
-	DDRB |= 0x1F; // bits 0-4 output
-
-	Serial.begin(115200);
-	Serial.println("Setup OK");
+byte readData(word addr) {
+	PORTC |= 0x01;
+	writeAddress(addr);
+	PORTC &= ~0x01;
+	_NOP();
+	_NOP();
+	_NOP();
+	_NOP();
+	_NOP();
+	byte data = ((PIND & 0xF0) >> 4) | ((PINB & 0x0F) << 4);
+	PORTC |= 0x01;
+	return data;
 }
 
 void display16(word data) {
@@ -51,23 +58,70 @@ void display16(word data) {
 	Serial.print( data        & 0xF, HEX);
 }
 
-void dump8(word from) {
+word currentAddr = 0;
+
+void read1(word from) {
+	currentAddr = from;
+	display16(from);
+	Serial.print(" : ");
+
+	byte data = readData(from);
+	Serial.print(data >> 4, HEX);
+	Serial.print(data & 0xF, HEX);
+
+	Serial.println();
+}
+
+void dump16(word from) {
+	currentAddr = from;
+
 	display16(from);
 	Serial.print(" :");
-	for (byte count=8;  count; from++, count--) {
+	for (byte count=16;  count; from++, count--) {
 		byte data = readData(from);
+		if (count == 8) {
+			Serial.print(' ');
+		}
 		Serial.print(' ');
 		Serial.print(data >> 4, HEX);
 		Serial.print(data & 0xF, HEX);
 	}
 	Serial.println();
 }
-void loop() {
-	// TODO : input
-	// r addr => read one data
-	// d addr => dump N data
-	for(word addr = 0x1200; addr < 0x1220; addr +=8) {
-		dump8(addr);
+
+void dump(word count) {
+	for(;;) {
+		dump16(currentAddr);
+		if (count < 16) {
+			currentAddr += count;
+			break;
+		}
+		currentAddr += 16;
+		count -= 16;
 	}
-	delay(5000);
+}
+
+InputItem inputs[] = {
+	{ 'r', 'I', (void *)read1 },
+	{ 'd', 'I', (void *)dump16 },
+	{ 'D', 'I', (void *)dump }
+};
+
+void setup() {
+	Serial.begin(115200);
+
+	DDRC |= 0x0F; // bits 0-4 output
+
+	PORTD&= ~0xF0; // bits 4-7 input without pull-up
+	DDRD &= ~0xF0;
+	PORTB&= ~0x0F; // bits 0-3 input without pull-up
+	DDRB &= ~0x0F;
+
+	registerInput(sizeof(inputs), inputs);
+
+	Serial.println("Setup OK");
+}
+
+void loop() {
+	handleInput();
 }
