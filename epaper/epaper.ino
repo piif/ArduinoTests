652539ff3ca8e347dd40d15c78487830fbefc851
Christian Lefebvre <christian_lefebvre@laposte.net>
2023-06-10 13:34:30 +0200

    first tests

---
+++ epaper/epaper.ino
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+#define EPAPER_BUSY     
+#define EPAPER_RESET
+#define EPAPER_DC
+#define EPAPER_CS
+#define EPAPER_SCLK
+#define EPAPER_SDI
+
+void setup() {
+}
+void loop() {
+}
