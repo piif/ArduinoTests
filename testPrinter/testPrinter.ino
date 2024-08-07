cbe3d41564647421433e02768c34e0ed6ff1db6e
Christian Lefebvre <christian_lefebvre@laposte.net>
2024-08-07 23:03:50 +0200

    PWM on enable pin instead of motor one

--- testPrinter/testPrinter.ino
+++ testPrinter/testPrinter.ino
@@ -6,8 +6,8 @@
 #endif
 
 #define M_X_EN  3
-#define M_X_A  5
-#define M_X_B  6
+#define M_X_A  4
+#define M_X_B  5
 
 #define M_Y_EN 11
 #define M_Y_A  9
@@ -19,6 +19,8 @@
 #define FORK_Y_A A2
 #define FORK_Y_B A3
 
+#define FORK_P 6
+
 int speedX = 0, speedY = 0;
 volatile byte forkX = 0, forkY = 0;
 volatile long posX = 0, posY = 0;
@@ -29,13 +31,14 @@ void setSpeedX(int v) {
         digitalWrite(M_X_EN, 0);
     } else {
         if (speedX > 0) {
-            analogWrite(M_X_A, speedX);
+            analogWrite(M_X_EN, speedX);
+            digitalWrite(M_X_A, 1);
             digitalWrite(M_X_B, 0);
         } else {
+            analogWrite(M_X_EN, -speedX);
             digitalWrite(M_X_A, 0);
-            analogWrite(M_X_B, -speedX);
+            digitalWrite(M_X_B, 1);
         }
-        digitalWrite(M_X_EN, 1);
     }
 }
 
@@ -45,17 +48,20 @@ void setSpeedY(int v) {
         digitalWrite(M_Y_EN, 0);
     } else {
         if (speedY > 0) {
-            analogWrite(M_Y_A, speedY);
+            analogWrite(M_Y_EN, speedY);
+            digitalWrite(M_Y_A, 1);
             digitalWrite(M_Y_B, 0);
         } else {
+            analogWrite(M_Y_EN, -speedY);
             digitalWrite(M_Y_A, 0);
-            analogWrite(M_Y_B, -speedY);
+            digitalWrite(M_Y_B, 1);
         }
-        digitalWrite(M_Y_EN, 1);
     }
 }
 
 ISR(PCINT1_vect) {
+    // TODO : ajouter un default pour empiler les erreurs dans un tableau
+    // et l'afficher avec le status 
     byte fX = ((digitalRead(FORK_X_A) & 1) << 1) | (digitalRead(FORK_X_B) & 1);
     if (fX != forkX) {
         switch (fX << 2 | forkX) {
@@ -126,6 +132,8 @@ void setup() {
 	pinMode(FORK_Y_A, INPUT);
 	pinMode(FORK_Y_B, INPUT);
 
+	pinMode(FORK_P, INPUT_PULLUP);
+
     // Listen PCINT 8,9,10,11 == interrupts on PC0,1,2,3 == A0,1,2,3
     PCMSK0 = 0;
     PCMSK1 = _BV(PCINT8) | _BV(PCINT9) | _BV(PCINT10) | _BV(PCINT11);
@@ -134,6 +142,9 @@ void setup() {
 
     forkX = ((digitalRead(FORK_X_A) & 1) << 1) | (digitalRead(FORK_X_B) & 1);
     forkY = ((digitalRead(FORK_Y_A) & 1) << 1) | (digitalRead(FORK_Y_B) & 1);
+
+// TODO : voir https://github.com/leomil72/analogComp/blob/master/analogComp.cpp pour implementer un analog comparator
+// entre 7 et A5 sur lequel on mettra un pont diviseur pour fixer une tension de seuil
 
 	registerInput(sizeof(inputs), inputs);
 	Serial.println("setup ok");
