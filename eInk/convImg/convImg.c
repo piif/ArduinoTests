97639a27346ffe8db2f6b767506e6467307eb730
Christian Lefebvre <christian_lefebvre@laposte.net>
2025-06-29 10:33:10 +0200

    output correct .h file

--- eInk/convImg/convImg.c
+++ eInk/convImg/convImg.c
@@ -12,16 +12,24 @@ void output(char chr, char count) {
     total_count++;
 }
 
-void main() {
-    printf("Image4b TO_REPLACE = {\n");
-    printf("    compressed_data = {\n");
+void main(int argc, char **argv) {
+    char *name="TO_REPLACE";
+    if (argc == 2) {
+        name = argv[1];
+    }
+    printf(
+        "#include \"image4b.h\"\n"
+        "\n"
+        "unsigned char %s_image_data[] = {\n",
+        name
+    );
     char l[100];
     int w = 0, h = 0;
     while(fgets(l, 99, stdin)) {
         char prev = l[0];
         char count = 0;
         int i;
-        printf("        ");
+        printf("    ");
         for (i = 1; l[i] != '\0' && l[i] != ' '; i++) {
             if (l[i]==prev) {
                 count++;
@@ -38,9 +46,15 @@ void main() {
         putchar('\n');
         h++;
     }
-    printf("    },\n");
-    printf("    width: %d,\n", w);
-    printf("    height: %d,\n", h);
-    printf("    data_len: %d\n", total_count);
-    printf("};\n");
+    printf(
+        "};\n"
+        "\n"
+        "Image4b %s_image = {\n"
+        "    width: %d,\n"
+        "    height: %d,\n"
+        "    data_len: %d,\n"
+        "    compressed_data: %s_image_data\n"
+        "};\n",
+        name, w, h, total_count, name
+    );
 }
